f7910ff990c5da0108a54df9cf1bac869292983c
Michal <michal.piotr.loboda@gmail.com>
2024-03-02 20:29:29 +0100

    garman klass indicator added

---
+++ indicators/tech_indicators.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def GK_vol(df):
+    df['GK_vol'] = ((np.log(df['high']) - np.log(df['low'])) **2) / 2 - (2 * np.log(2) - 1) * ((np.log(df['adj close']) - np.log(df['open']))**2)
+    return df
