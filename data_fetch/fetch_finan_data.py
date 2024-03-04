156e5c97d5dc438d0c7c004871e516ab131b0213
Michal <michal.piotr.loboda@gmail.com>
2024-03-04 10:29:13 +0100

    change to csv

--- data_fetch/fetch_finan_data.py
+++ data_fetch/fetch_finan_data.py
@@ -2,13 +2,15 @@ import yfinance as yf
 import pandas as pd
 import datetime as dt
 
-def historic_data(tickers, years):
+def historic_data(name, tickers, days):
     end_date = dt.date.today()
-    start_date = pd.to_datetime(end_date) - pd.Timedelta(days=365*years)
+    start_date = pd.to_datetime(end_date) - pd.Timedelta(days=days)
     
     data_df = yf.download(tickers, start=start_date, end=end_date).stack(future_stack=True)
 
     data_df.index.names = ['date', 'ticker']
     data_df.columns = data_df.columns.str.lower()
+
+    data_df.to_excel(f'../data/historical_{name}_{days}days.xlsx')
 
     return data_df
