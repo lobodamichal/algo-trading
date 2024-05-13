4561ac742080a7c0a1b4f6144d674ae094d99081
Michal <michal.piotr.loboda@gmail.com>
2024-05-13 11:00:47 +0200

    script edit

--- script.py
+++ script.py
@@ -6,49 +6,12 @@ def testPlot():
     sp500 = Index('sp500')
 
     sp500.fetch_tickers(sp500.sp500_url)
-    #sp500.fetch_hist_financial_data(days=4*365, path='./data/sp500.xlsx')
+    sp500.fetch_hist_financial_data(days=4*365, path='./data/sp500.xlsx')
     sp500.read_hist_financial_data(path='./data/sp500.xlsx')
     stocks_dict = sp500.generate_stocks()
 
     plot = Plot()
-    plot.plot(stocks_dict['AAPL'], days=90)
-
-def createTable():
-    ddb = bt.resource('dynamodb',
-                        endpoint_url='http://localhost:8000',
-                        region_name='dummy',
-                        aws_access_key_id='dummy',
-                        aws_secret_access_key='dummy')
-
-    # Create the DynamoDB table.
-    ddb.create_table(
-            TableName='test',
-            AttributeDefinitions=[
-                {
-                    'AttributeName': 'date',
-                    'AttributeType': 'S'
-                }
-            ],
-            KeySchema=[
-                {
-                    'AttributeName': 'date',
-                    'KeyType': 'HASH'
-                }
-            ],
-            ProvisionedThroughput={
-                'ReadCapacityUnits': 10,
-                'WriteCapacityUnits': 10
-            }
-        )
-
-    table = ddb.Table('test')
-    input={'date': '2024-03-12', 'ticker': 'TST', 'close': 1234}
-    table.put_item(Item=input)
-
-    scanResponse = table.scan(TableName='test')
-    items = scanResponse['Items']
-    for item in items:
-        print(item)
+    plot.plot(stocks_dict['AAPL'], days=1)
 
 def testPortfolio():
     sp500 = Index('sp500')
